97d446c1697e2ad5a2d3ec0a10022852692381fd
Lalith aditya Chunduri <chun8135@vandals.uidaho.edu>
2025-08-04 13:07:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-white text-gray-800 text-3xl">
-      Whisper AI is Live 💬
-    </div>
-  )
-}
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
